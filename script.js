9205006cd0bfb685e7a3874b1101dd78f5018f80
VedDeshmukh02 <veddeshmukh550@gmail.com>
2026-01-03 21:15:10 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+const checkbox = document.getElementById('notescb');
+const inputs = document.getElementsByClassName('inputs');
+
+const today = new Date().toDateString();
+const savedDate = localStorage.getItem('savedDate')
+
+if (savedDate !== today) {
+    for (let input of inputs) {
+        input.value = ''
+    }
+    checkbox.checked = false;
+
+    localStorage.clear();
+    localStorage.setItem('savedDate', today);
+} 
+
+checkbox.checked = localStorage.getItem('checkState') === 'true';
+
+checkbox.addEventListener('change', () => {
+    localStorage.setItem('checkState', checkbox.checked);
+});
+
+for (let i = 0; i < inputs.length; i++) {
+    const savedValue = localStorage.getItem('input-' + i);
+    if (savedValue !== null) {
+        inputs[i].value = savedValue;
+    }
+     inputs[i].addEventListener('input', function() {
+        localStorage.setItem('input-' + i, this.value);
+     });
+}
+
+let password = document.getElementById('password');
+let boxes = document.getElementById('boxes');
+let passdiv = document.getElementById('security')
+
+function checkPassword() {
+    if (password.value == 'ABC@128') {
+        boxes.style.display = 'grid';
+        passdiv.style.display = 'none';
+    }
+}
